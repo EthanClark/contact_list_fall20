3026109450e138d7c89c1babede4e355eb0be50a
Ethan Clark <clark.ethan.r@gmail.com>
2020-09-01 19:40:53 -0600

    added notes and addresses

---
+++ app/models/contact.rb
@@ -0,0 +1,11 @@
+class Contact < ApplicationRecord
+  has_many :notes, dependent: :destroy
+  has_one :address, dependent: :destroy
+  # Associations 
+  # belongs_to - childs
+  # has_many - parent
+  # has_one - parent
+  # has_many :through
+  # has_one :through
+  # dependent, has_one, has_many
+end
